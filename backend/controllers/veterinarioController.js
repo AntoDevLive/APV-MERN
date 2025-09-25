b0801e21a2183645933e47c98fb01e0674f5eae5
antoniocp99 <antoniocastizo99@gmail.com>
2025-09-25 23:58:52 +0200

    validar usuario via token

--- backend/controllers/veterinarioController.js
+++ backend/controllers/veterinarioController.js
@@ -27,8 +27,25 @@ const perfil = (req, res) => {
     res.json({ url: 'desde perfil' })
 }
 
-const confirmar = (req, res) => {
-    console.log(req.params.token)
+const confirmar = async(req, res) => {
+    const { token } = req.params;
+
+    const usuarioConfirmar = await Veterinario.findOne({token})
+
+    if(!usuarioConfirmar) {
+        const error = new Error('Token no válido')
+        return res.status(404).json({msg: error.message})
+    }
+
+    try {
+        usuarioConfirmar.token = null;
+        usuarioConfirmar.confirmado = true;
+        await usuarioConfirmar.save();
+
+        res.json({msg: 'cuenta validada'})
+    } catch(error) {
+        console.log(error)
+    }
 
     res.json({ url: 'confirmado' })
 }
